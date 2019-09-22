04b2088b8896a54b5d0604a8914349222772c198
Guy Grigsby <guy.grigsby@ahs.com>
2019-09-21 20:44:10 -0600

    add other pages

---
+++ src/gatsby-theme-blog/components/home-footer.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react"
+import { Styled, css } from "theme-ui"
+import { Link } from "gatsby"
+
+const Footer = ({ socialLinks }) => (
+  <footer
+    css={css({
+      mt: 4,
+      pt: 3,
+    })}
+  >
+    <Link to="/single/">Mystery Site</Link>
+    <Fragment>
+      {` `}&bull;{` `}
+    </Fragment>
+    {socialLinks.map((platform, i, arr) => (
+      <Fragment key={platform.url}>
+        <Styled.a href={platform.url} target="_blank" rel="noopener noreferrer">
+          {platform.name}
+        </Styled.a>
+        {arr.length - 1 !== i && (
+          <Fragment>
+            {` `}&bull;{` `}
+          </Fragment>
+        )}
+      </Fragment>
+    ))}
+  </footer>
+)
+export default Footer
